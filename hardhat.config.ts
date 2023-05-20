369d18a228c9ac9012a26d111ab83ef74a56c2aa
Nicolás Santángelo <santangelonicolas@gmail.com>
2023-05-19 21:20:36 -0300

    Support eth_accounts and eth_sign/personal_sign

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-import { ProviderWrapper } from "hardhat/internal/core/providers/wrapper";
-import { extendProvider } from "hardhat/config";
-import { EIP1193Provider, RequestArguments } from "hardhat/types";
+import { ProviderWrapper } from "hardhat/plugins";
+import { HardhatUserConfig, extendProvider } from "hardhat/config";
+import {
+  EIP1193Provider,
+  HardhatConfig,
+  RequestArguments,
+} from "hardhat/types";
 
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
 import { TransportError } from "@ledgerhq/errors";
 import Eth from "@ledgerhq/hw-app-eth";
+
+import { validateParams } from "hardhat/internal/core/jsonrpc/types/input/validation";
+import {
+  rpcAddress,
+  rpcData,
+} from "hardhat/internal/core/jsonrpc/types/base-types";
+import { HardhatError } from "hardhat/src/internal/core/errors";
+import { ERRORS } from "hardhat/src/internal/core/errors-list";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
@@ -20,13 +31,22 @@ export interface LedgerOptions {
   connectionTimeout?: number;
 }
 
-class LedgerProvider extends ProviderWrapper implements EIP1193Provider {
+class LedgerProvider extends ProviderWrapper {
   public static readonly DEFAULT_TIMEOUT = 3000;
 
   public name: string = "LedgerProvider";
 
-  private eth: Eth | undefined;
-  private isCreatingTransport = false;
+  private _eth: Eth | undefined;
+  private _isCreatingTransport = false;
+
+  static async create(
+    options: LedgerOptions,
+    _wrappedProvider: EIP1193Provider
+  ) {
+    const provider = new LedgerProvider(options, _wrappedProvider);
+    await provider.init();
+    return provider;
+  }
 
   constructor(
     public readonly options: LedgerOptions,
@@ -36,17 +56,78 @@ class LedgerProvider extends ProviderWrapper implements EIP1193Provider {
   }
 
   public async request(args: RequestArguments): Promise<unknown> {
-    // this.signTransaction(...)
-    // (hre.ethers || ethers).provider.sendTransaction
+    const { ecsign, hashPersonalMessage, toRpcSig, toBuffer, bufferToHex } =
+      await import("@nomicfoundation/ethereumjs-util");
+
+    const params = this._getParams(args);
+
+    if (this._eth === undefined) {
+      throw new Error();
+    }
+
+    // get Ethereum address for a given BIP 32 path.
+    if (
+      args.method === "eth_accounts" ||
+      args.method === "eth_requestAccounts"
+    ) {
+      const wallet = await this._eth.getAddress(this.options.path);
+      return [wallet.address];
+    }
+
+    // You can sign a transaction and retrieve v, r, s given the raw transaction and the BIP 32 path of the account to sign.
+    // const tx = "e8018504e3b292008252089428ee52a8f3d6e5d15f8b131996950d7f296c7952872bd72a2487400080"; // raw tx to sign
+    // const resolution = await ledgerService.resolveTransaction(tx);
+    // const result = eth.signTransaction("44'/60'/0'/0/0", tx, resolution);
+    // console.log(result);
+
+    // You can sign a message according to eth_sign RPC call and retrieve v, r, s given the message and the BIP 32 path of the account to sign.
+    if (args.method === "personal_sign" || args.method === "eth_sign") {
+      if (params.length > 0) {
+        let data: Buffer;
+        let address: Buffer;
+
+        if (args.method === "personal_sign") {
+          const validParams = validateParams(params, rpcData, rpcAddress);
+          data = validParams[0];
+          address = validParams[1];
+        } else {
+          // eth_sign
+          const validParams = validateParams(params, rpcAddress, rpcData);
+          data = validParams[1];
+          address = validParams[0];
+        }
+
+        if (data !== undefined) {
+          if (address === undefined) {
+            throw new HardhatError(
+              ERRORS.NETWORK.PERSONALSIGN_MISSING_ADDRESS_PARAM
+            );
+          }
+
+          const signature = await this._eth.signPersonalMessage(
+            this.options.path,
+            data.toString("hex")
+          );
+
+          return toRpcSig(
+            BigInt(signature.v - 27),
+            toBuffer("0x" + signature.r),
+            toBuffer("0x" + signature.s)
+          );
+        }
+      }
+    }
+
+    // Sign a prepared message following web3.eth.signTypedData specification. The host computes the domain separator and hashStruct(message)
+    // eth.signEIP712HashedMessage("44'/60'/0'/0/0", Buffer.from("0101010101010101010101010101010101010101010101010101010101010101").toString("hex"), Buffer.from("0202020202020202020202020202020202020202020202020202020202020202").toString("hex"))
 
     // throw if not init?
-
     return this._wrappedProvider.request(args);
   }
 
   public async init() {
-    if (!this.eth && !this.isCreatingTransport) {
-      this.isCreatingTransport = true;
+    if (!this._eth && !this._isCreatingTransport) {
+      this._isCreatingTransport = true;
       const openTimeout =
         this.options.openTimeout || LedgerProvider.DEFAULT_TIMEOUT;
       const connectionTimeout =
@@ -57,7 +138,7 @@ class LedgerProvider extends ProviderWrapper implements EIP1193Provider {
           openTimeout,
           connectionTimeout
         );
-        this.eth = new Eth(transport);
+        this._eth = new Eth(transport);
       } catch (error) {
         if (error instanceof Error) {
           let errorMessage = `There was an error trying to stablish a connection to the Ledger wallet: "${error.message}".`;
@@ -75,40 +156,13 @@ class LedgerProvider extends ProviderWrapper implements EIP1193Provider {
 
         throw error;
       }
-      this.isCreatingTransport = false;
+      this._isCreatingTransport = false;
     }
   }
 }
 
-// private async signTransaction(
-//   transaction: ethers.providers.TransactionRequest
-// ): Promise<string> {
-//   const tx =
-//     await hre.ethers.utils.resolveProperties<ethers.providers.TransactionRequest>(
-//       transaction
-//     );
-//   const baseTx: ethers.utils.UnsignedTransaction = {
-//     chainId: tx.chainId || undefined,
-//     data: tx.data || undefined,
-//     gasLimit: tx.gasLimit || undefined,
-//     gasPrice: tx.gasPrice || undefined,
-//     nonce: tx.nonce ? ethers.BigNumber.from(tx.nonce).toNumber() : undefined,
-//     to: tx.to || undefined,
-//     value: tx.value || undefined,
-//   };
-
-//   const unsignedTx = ethers.utils.serializeTransaction(baseTx).substring(2);
-//   const sig = await this.eth?.signTransaction(this.path, unsignedTx)!;
-
-//   return ethers.utils.serializeTransaction(baseTx, {
-//     v: ethers.BigNumber.from("0x" + sig.v).toNumber(),
-//     r: "0x" + sig.r,
-//     s: "0x" + sig.s,
-//   });
-// }
-
-// extendProvider(async (provider: EIP1193Provider) => {
-//   const newProvider = new LedgerProvider({ path: "44'/60'/0'/0/0" }, provider);
-//   await newProvider.init();
-//   return newProvider;
-// });
+extendProvider(
+  async (provider: EIP1193Provider, config: HardhatConfig, network: string) => {
+    return await LedgerProvider.create({ path: "44'/60'/0'/0" }, provider);
+  }
+);
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,40 @@
 import hre from "hardhat";
 
 async function main() {
-  const chainId = await hre.network.provider.request({ method: "eth_chainId" });
-  hre.network.provider.on("change", () => {});
-  console.log("Chain id", chainId);
+  const { provider } = hre.network;
+
+  const [account] = (await provider.request({
+    method: "eth_accounts",
+  })) as string[];
+  console.log("METAMASK Account: 0x8f649fe750340a295dddbbd7e1ec8f378cf24b42");
+  console.log("Account", account);
+  console.log("\n");
+
+  const personalSignResult = await provider.request({
+    method: "personal_sign",
+    params: [
+      "0x5417aa2a18a44da0675524453ff108c545382f0d7e26605c56bba47c21b5e979",
+      account,
+    ],
+  });
+  console.log(
+    "METAMASK      Result: 0xdfbab2781f2b3086a954d05c8924e1f047cc85e18c6640a6077f4e2cae93f15b4bc225b8d7692da2d6e80f41edf0abfa1c9fb8300652ee3ece056787acda31ad00"
+  );
+  console.log("personal_sign Result:", personalSignResult);
+  console.log("\n");
+
+  const ethSignResult = await provider.request({
+    method: "eth_sign",
+    params: [
+      account,
+      "0x7699f568ecd7753e6ddf75a42fa4c2cc86cbbdc704c9eb1a6b6d4b9d8b8d1519",
+    ],
+  });
+  console.log(
+    "METAMASK Result: 0x095655e777e3c940cc1e9a509d584f73b1aea4edbb7722ddf830a9e0f8b2fc67478532aba93736e600250814a3b087a5ac179e5f10965aa543faa6b943117cf301"
+  );
+  console.log("eth_sign Result:", ethSignResult);
+  console.log("\n");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
